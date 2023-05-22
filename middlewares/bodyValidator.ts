b68cb9290bb792304602336f547fcc6fd3535d75
Esneider Ospina Oviedo <sneuder8@gmail.com>
2023-05-22 12:22:50 -0500

    feat: body validator for decrypt

--- middlewares/bodyValidator.ts
+++ middlewares/bodyValidator.ts
@@ -2,15 +2,23 @@ import Joi from 'joi'
 import { Response, Request, NextFunction } from 'express'
 import msg from '../services/msg'
 
-const schema = Joi.object({
-  words: Joi.string().required(),
-  keywords: Joi.string().required(),
-})
+const schemas = {
+  encrypt: Joi.object({
+    words: Joi.string().required(),
+    keywords: Joi.string().required(),
+  }),
+  decrypt: Joi.object({
+    decrypted: Joi.string().required(),
+    keywords: Joi.string().required(),
+  }),
+}
 
-function bodyValidator(req: Request, res: Response, next: NextFunction) {
-  const validatation = schema.validate(req.body)
-  if (!validatation.error) return next()
-  res.status(400).json(msg.send(false, validatation.error.details[0].message))
+function bodyValidator(schema: 'encrypt' | 'decrypt') {
+  return (req: Request, res: Response, next: NextFunction) => {
+    const validatation = schemas[schema].validate(req.body)
+    if (!validatation.error) return next()
+    res.status(400).json(msg.send(false, validatation.error.details[0].message))
+  }
 }
 
 export default bodyValidator
--- routes/index.ts
+++ routes/index.ts
@@ -4,8 +4,13 @@ import bodyValidator from '../middlewares/bodyValidator'
 import keywordsValidator from '../middlewares/keywordsValidator'
 
 function routes(router: Router) {
-  router.post('/encrypt', bodyValidator, encryptController)
-  router.post('/decrypt', keywordsValidator, decryptController)
+  router.post('/encrypt', bodyValidator('encrypt'), encryptController)
+  router.post(
+    '/decrypt',
+    bodyValidator('decrypt'),
+    keywordsValidator,
+    decryptController
+  )
 }
 
 export default routes
